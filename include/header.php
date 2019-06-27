2681220547be728622068af3599bce51b5f7e8cc
David Teixeira <david.teixeira31@gmail.com>
2019-06-27 11:06:01 +0200

    add navbar on php

--- include/header.php
+++ include/header.php
@@ -7,6 +7,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/navbar.css">
 
     <?php if ($_SERVER["SCRIPT_NAME"] === "/projet-php/login.php") : ?>
         <link rel="stylesheet" href="css/login.css">
@@ -15,5 +16,7 @@
 </head>
 
 <body>
-
+    <header>
+        <?php require_once("include/nav.php") ?>
+    </header>
     <main>
--- include/nav.php
+++ include/nav.php
@@ -1,33 +1,17 @@
 <div class="nav-header">
-
-    <a href="home.php" class="nav-logo"><img src="images/logo.png" alt=""></a>
-
     <div class="nav">
         <ul class="nav-menu">
-            <li><a href="food-supplements.php">Nutrition</a></li>
-            <?php if (isset($_SESSION["connectedUser"]) && $_SESSION["connectedUser"]) :
-                if (empty($premium[0]['id_premium'])) : ?>
-                    <li><a href="profil.php">Profil</a></li>
-                    <li><a href="home.php#premium">Premium</a></li>
-                <?php else : ?>
-                    <li><a href="profil.php">Profil</a></li>
-                    <li><a href="coach.php">Coach</a></li>
-                <?php endif; ?>
-                <li><a href="meeting.php">Seance</a></li>
-
-            <?php elseif (isset($_SESSION["connectedCoach"]) && $_SESSION["connectedCoach"]) : ?>
-                <li><a href="profil.php">Profil</a></li>
-                <li><a href="client.php">Client</a></li>
-            <?php endif; ?>
+            <li><a href="">Accueil</a></li>
+            <li><a href="">A remplir</a></li>
+            <li><a href="">Événements</a></li>
+            <li><a href="">Profil</a></li>
         </ul>
-
         <ul class="nav-login">
-            <?php if (isset($_SESSION["connectedUser"]) && $_SESSION["connectedUser"] || isset($_SESSION["connectedCoach"]) && $_SESSION["connectedCoach"]) : ?>
+            <?php if ($_SESSION["connected"]) : ?>
                 <li><a href="disconnection.php">Deconnexion</a></li>
             <?php else : ?>
                 <li><a href="login.php">Se connecter</a></li>
             <?php endif; ?>
-
         </ul>
     </div>
     <div class="menu-burger">
