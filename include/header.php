89c3d30d344b83ed08a42c3f0bc9633260f34f91
David Teixeira <david.teixeira31@gmail.com>
2019-06-27 10:21:31 +0200

    add login on php

---
+++ include/header.php
@@ -0,0 +1,19 @@
+<?php require_once("include/function.php") ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <?php if ($_SERVER["SCRIPT_NAME"] === "/projet-php/login.php") : ?>
+        <link rel="stylesheet" href="css/login.css">
+        <title>Connexion</title>
+    <?php endif; ?>
+</head>
+
+<body>
+
+    <main>
---
+++ login.php
@@ -0,0 +1,19 @@
+<?php require_once("include/header.php") ?>
+
+<div class="login-container">
+    <form action="" method="POST" class="login-box">
+        <h1>Login</h1>
+        <div class="border"></div>
+        <div class="login">
+            <div class="user-infos">
+                <input class="login-info" type="text" name="login" placeholder="Username">
+                <input class="login-info" type="password" name="password" placeholder="Password">
+                <input class="login-submit" type="submit" id="" value="Login">
+                <a href="signup.php">Vous n'avez pas de compte créez en un !<br></a>
+                <a href="">Mot de passe oublié ?<br> </a>
+            </div>
+        </div>
+    </form>
+</div>
+
+<?php require_once("include/footer.php") ?>
