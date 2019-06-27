e377ff1210a17db2f0f11020bea262302b2e1172
David Teixeira <david.teixeira31@gmail.com>
2019-06-27 14:15:06 +0200

    merge

--- include/header.php
+++ include/header.php
@@ -12,20 +12,17 @@
     <?php if ($_SERVER["SCRIPT_NAME"] === "/projet-php/login.php") : ?>
         <link rel="stylesheet" href="css/login.css">
         <title>Connexion</title>
-<<<<<<< HEAD
     <?php elseif ($_SERVER["SCRIPT_NAME"] === "/projet-php/profil.php") : ?>
         <link rel="stylesheet" href="css/profil.css">
         <title>Votre Profil</title>
-=======
     <?php elseif ($_SERVER["SCRIPT_NAME"] === "/projet-php/signup.php") : ?>
         <link rel="stylesheet" href="css/signup.css">
         <title>inscription</title>
->>>>>>> 969eb0e3e0764e411219a87d33002a0a0c5648f6
     <?php endif; ?>
-    </head>
+</head>
 
-    <body>
-        <header>
-            <?php require_once("include/nav.php") ?>
-        </header>
-        <main>
+<body>
+    <header>
+        <?php require_once("include/nav.php") ?>
+    </header>
+    <main>
