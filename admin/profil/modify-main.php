01c10d814bc20d938bf81b29cb109566412ac5d8
David Teixeira <david.teixeira31@gmail.com>
2019-06-27 16:27:05 +0200

    modify files admin

--- admin/profil/modify-main.php
+++ admin/profil/modify-main.php
@@ -1,3 +1,17 @@
 <?php require_once("../../include/header.php") ?>
 
+<form action="" method="post" enctype="multipart/form-data">
+    <input type="file" name="image" id="">
+    <input type="text" name="firstname" id="" placeholder="Prénom">
+    <input type="text" name="lastname" id="" placeholder="Nom">
+    <SELECT class="" name="age" size="1">
+        <OPTION> Age
+            <?php for ($i = 16; $i <= 100; $i++) : ?>
+            <OPTION><?= $i ?>
+            <?php endfor ?>
+    </SELECT>
+    <textarea name="description" id="" cols="30" rows="10"></textarea>
+    <button type="submit">Valider</button>
+</form>
+
 <?php require_once("../../include/footer.php") ?>
--- admin/profil/modify-password.php
+++ admin/profil/modify-password.php
@@ -1,3 +1,8 @@
 <?php require_once("../../include/header.php") ?>
 
+<form action="" method="post" enctype="multipart/form-data">
+    <input type="password" name="password" id="" placeholder="Password">
+    <button type="submit">Valider</button>
+</form>
+
 <?php require_once("../../include/footer.php") ?>
