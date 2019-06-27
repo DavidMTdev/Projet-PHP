6cdd1eaa878993af1a4bbd82e86527502c0da7c2
David Teixeira <david.teixeira31@gmail.com>
2019-06-27 10:51:29 +0200

    add fct login

--- include/function.php
+++ include/function.php
@@ -3,7 +3,12 @@ session_start();
 
 function getPdo()
 {
-    $pdo = new PDO("mysql:host=localhost:3306;dbname=portfolio", "root", "");
+    try {
+        $pdo = new PDO("mysql:host=localhost:3306;dbname=projet_php", "root", "");
+    } catch (PDOException $e) {
+        var_dump($e);
+    }
+
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     return $pdo;
 }
@@ -30,4 +35,28 @@ function execute($sql, $args = array())
     $stmt = $pdo->prepare($sql);
     $stmt->execute($args);
     return $pdo->lastInsertId();
+}
+
+if (isset($_POST["login"]) && isset($_POST["password"])) {
+
+    $login = $_POST["login"];
+    $password = $_POST["password"];
+
+    $user = selectOne("SELECT * FROM user WHERE mail_user = :login", array(
+        'login' => $login
+    ));
+
+    if ($user) {
+        if (password_verify($password, $user["password"])) {
+            $_SESSION["login"] = $_POST["login"];
+            $_SESSION["connected"] = true;
+            $_SESSION["connectedAt"] = new DateTime();
+
+            echo "Connecté avec succès !";
+        } else {
+            echo "Mot de passe incorrect";
+        }
+    } else {
+        echo "Aucun compte avec ce login";
+    }
 }
